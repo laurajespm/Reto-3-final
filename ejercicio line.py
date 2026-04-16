9aa2c2c1494e4d3093625de824e165d089fd0fc4
Laura Espinosa <laespinosa@unal.edu.co>
2026-04-15 20:13:53 -0500

    ejercicio line.py

--- ejercicio line.py
+++ ejercicio line.py
@@ -13,37 +13,56 @@ class Line:
         self.end = end
         self.length = 0
         self.slope = 0
-    #preguntar: ¿Podría acá usar una property de length?
+        
+    def calculate_m(self):
+        if self.end.x == self.start.x:
+            return None
+        return ((self.end.y - self.start.y)/(self.end.x - self.start.x))
+            
     def compute_length(self):
-        return math.hypot((self.end.x - self.start.x),(self.end.y - self.start.y))
+        self.length = math.hypot((self.end.x - self.start.x),(self.end.y - self.start.y))
+        return self.length
     
     def compute_slope(self):
-        if (self.end.x - self.start.x) == 0:
-            print ("La pendiente es indefinida, pues se trata de una línea vertical.")
-            return None
+        m = self.calculate_m()
+        if m is None:
+            self.slope = 90
         else:
-            return ((self.end.y - self.start.y)/(self.end.x - self.start.x))
+            self.slope = math.degrees(math.atan(m))
+        return self.slope
     
     def compute_horizontal_cross(self):
-        m = self.compute_slope()
+        if self.end.x == self.start.x:
+            return Point (self.start.x, 0)
+            
         if self.start.y == 0 and self.end.y == 0:
             print ("El cruce horizontal es infinito pues la línea se encuentra sobre el eje x.")
             return None
+            
         if (self.start.y == self.end.y):
             print ("La línea nunca cruza el eje x")
             return None
-        if self.end.x == self.start.x:
-            return Point (self.start.x, 0)
+            
+        m = self.calculate_m()
         return  Point ((self.start.x - (self.start.y / m)), 0)
 
-    def compute_vertical_cross(self):
-        m = self.compute_slope()
+    def compute_vertical_cross(self):   
+        if self.start.y == self.end.y:
+            return Point(0, self.start.y)
+            
         if self.start.x == 0 and self.end.x == 0:
             print ("El cruce vertical es infinito, pues la línea se encuentra sobre el eje y.")
             return None
+            
         if (self.start.x == self.end.x):
             print ("La línea nunca cruza el eje y")
             return None
+            
+        m = self.calculate_m()
+        if m is None:
+            return None
         return Point(0, (self.start.y - (m * self.start.x)))
+
+
 
         
